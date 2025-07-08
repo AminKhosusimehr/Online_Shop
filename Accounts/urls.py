58e706b00cfa817729d777530a333bce8486615b
AminKhosusimehr <khosusimehr@gamil.com>
2025-07-08 09:24:33 +0330

    feat : simple authentication and login

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,11 +1,11 @@
+from django.urls import path
+
+
+from .views import CustomTokenObtainPairView ,RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import RegisterView , TokenObtainPairView
-from django.urls import path
-app_name = 'Accounts'
-
-urlpatterns=[
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+urlpatterns = [
+    path('login/' , CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/' , TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/' , RegisterView.as_view(), name='register'),
 ]
