90d34f968bb7d5e89e079ad3329c0e591d1b768c
AminKhosusimehr <khosusimehr@gamil.com>
2025-07-10 17:52:33 +0330

    feat : Adding jwt authentication

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,9 +1,12 @@
-from .views import RegisterView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import RegisterView , TokenObtainPairView
 from django.urls import path
 
 app_name = 'Accounts'
 
 urlpatterns=[
     path('register/', RegisterView.as_view(), name='register'),
-
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
