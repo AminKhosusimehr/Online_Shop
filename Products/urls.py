0efee8c664cdfe1d70634928d5bc8f959edd7fb8
AminKhosusimehr <khosusimehr@gamil.com>
2025-08-02 13:40:36 +0330

    feat : Adding CRUD to product model

---
+++ Products/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from Products import views
+
+
+app_name = 'Products'
+
+router = DefaultRouter()
+
+router.register(r'products', views.ProductViewSet)
+
+urlpatterns = [
+    path('' , include(router.urls)),
+]
