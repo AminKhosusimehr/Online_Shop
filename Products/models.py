b1921a5ba74a72393f3f031f318e921846072bc8
AminKhosusimehr <khosusimehr@gamil.com>
2025-08-04 13:10:31 +0330

    feat : Adding stock field to product model

--- Products/models.py
+++ Products/models.py
@@ -4,6 +4,7 @@ class Product (models.Model):
     name = models.CharField(blank=False, null=False, max_length=50)
     price = models.PositiveIntegerField(blank=False, null=False)
     is_available = models.BooleanField(default=True)
+    stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
