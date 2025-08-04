c92c488e6e29f16733973932e0d7e204c2bfc153
AminKhosusimehr <khosusimehr@gamil.com>
2025-08-04 14:19:09 +0330

    feat : changing the default value of is_available

--- Products/models.py
+++ Products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product (models.Model):
     name = models.CharField(blank=False, null=False, max_length=50)
     price = models.PositiveIntegerField(blank=False, null=False)
-    is_available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=False)
     stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
