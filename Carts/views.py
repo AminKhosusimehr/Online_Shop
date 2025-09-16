dad0a536528626ca83a33356127054f6aad9e321
AminKhosusimehr <khosusimehr@gamil.com>
2025-09-16 16:06:12 +0330

    feat : Completing cart features

--- Carts/views.py
+++ Carts/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 import Carts
+from Carts.models import CartItem
+from Carts.serializers import CartItemSerializer
 
 
-class CartViewSet(viewsets.ModelViewSet):
-    class Meta :
-        model = Carts
+class CartItemViewSet(viewsets.ModelViewSet):
+        serializer_class = CartItemSerializer
+        permission_classes = [permissions.IsAuthenticated]
 
+        def get_queryset(self):
+                return CartItem.objects.filter(cart=self.request.user.cart_items)
+
+        def perform_create(self, serializer):
+                user = self.request.user
+                cart = getattr(user , 'cart_items', None)
+                serializer.save(cart=cart)
