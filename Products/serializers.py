cc4d64175c72edbc912333ec0d18d43d3a4ea8d6
AminKhosusimehr <khosusimehr@gamil.com>
2025-09-17 12:44:37 +0330

    feat : Adding validation for creating a new product

--- Products/serializers.py
+++ Products/serializers.py
@@ -8,3 +8,11 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
         read_only_fields = ('id',)
+
+    def validate(self, data):
+        is_available = data['is_available']
+        stock = data['stock']
+
+        if not is_available and stock > 0 :
+            raise serializers.ValidationError("The stock must be zero for an unavailable product")
+        return data
